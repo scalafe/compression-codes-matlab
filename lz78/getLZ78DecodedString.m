38059e78129f8a80d0fb423cde09aebc85c0fd4b
Felice Scala <scala.felice@hotmail.it>
2023-11-05 18:29:56 +0100

    Aggiunti commenti

--- lz78/getLZ78DecodedString.m
+++ lz78/getLZ78DecodedString.m
@@ -1,25 +1,38 @@
 function [result] = getLZ78DecodedString(input, encoded_simbols)
-%GETLZ78DECODEDSTRING Summary of this function goes here
-%   Detailed explanation goes here
+%GETLZ78DECODEDSTRING Decodifa una stringa codificata con LZ78
+%   Decodifica una stringa codificata con l'algoritmo LZ78.
+%   -   input è la stringa codificata
+%   -   encoded_simbols è il dizionario che associa ogni simbolo con la
+%   propria codifica
     arguments
         input
         encoded_simbols
     end
+    % Definizione funzione per il calcolo del numero di bit necessari per
+    % codificare num_elems elementi
     num_bits = @(num_elems) ceil(log2(num_elems));
 
+    % Calcolo del numero di bit necessari per codificare i simboli e gli
+    % indici
     num_bits_simbols = num_bits(numel(encoded_simbols.keys));
     num_bits_indexes = numel(input(1,:)) - num_bits_simbols;
 
-    decoded_simbols = dictionary(encoded_simbols.values, encoded_simbols.keys)
+    % Creazione del dizionario che associa ogni codifica al suo simbolo
+    % orginale, a partire da encoded_simbols
+    decoded_simbols = dictionary(encoded_simbols.values, encoded_simbols.keys);
 
+    % Inizializzazione dell'array di celle per conservare i risultati
     result = cell(size(input,1),1);
 
+    % Decodifica della stringa input
     for ii=1:length(result)
+        % Recupero tupla (father_pos, simb)
         encoded_index = num2str(input(ii, 1:num_bits_indexes));
         encoded_index = encoded_index(~isspace(encoded_index));
         encoded_simbol = input(ii, num_bits_indexes+1:end);
         simbol = decoded_simbols({encoded_simbol});
 
+        % Recupero della frase associata alla tupla
         index = bin2dec(encoded_index);
         if index == 0
             result(ii) = simbol;
@@ -27,7 +40,8 @@ function [result] = getLZ78DecodedString(input, encoded_simbols)
             result(ii) = {[cell2mat(result(index));cell2mat(simbol)]};
         end
     end
-
+   
+    % Conversione dell'array di celle in una matrice
     result = cell2mat(result);
 end
 
--- lz78/getLZ78EncodedString.m
+++ lz78/getLZ78EncodedString.m
@@ -1,28 +1,45 @@
 function [result, simbols, encoded_simbols, tuples] = getLZ78EncodedString(input)
-%GETLZ78ENCODEDSTRING Summary of this function goes here
-%   Detailed explanation goes here
+%GETLZ78ENCODEDSTRING Codifca una stringa di simboli
+%   Codifca una stringa di simboli utilizzando l'algoritmo LZ78
+%   Parametri:
+%   -   input è la stringa da codificare
+%   Valori di ritorno:
+%   -   result è la stringa codificata;
+%   -   simbols è il vettore con tutti i simboli elementari trovati durante
+%   l'esecuzione;
+%   -   encoded_simbols è il dizionario che associa ogni simbolo con la
+%   codifica assegnatagli;
+%   -   tuples è l'array di celle che contiene le coppie (father_pos, simb)
+%   che definiscono le frasi individuate durante il parsing
     arguments
         input
     end
     num_bits = @(num_elems) ceil(log2(num_elems));
 
+    % Recupero delle tuple delle frasi dalla stringa
     [tuples, simbols, num_phrases] = getLZ78EncodedTuples(input);
 
+    % Recupero della massimo valore di father_pos delle tuple
     max_tuple_index = max(cell2mat(tuples(:,1)));
 
+    % Calcolo del numero di bit necessari per codificare gli indici e i
+    % simboli in binario
     num_bits_indexes = num_bits(max_tuple_index + 1);
     num_bits_simbols = num_bits(numel(simbols(:,1)));
 
+    % Inizializzazione dell'array finale e del dizionario delle codifiche
+    % dei simboli
     result = zeros(num_phrases, num_bits_simbols + num_bits_indexes);
-
     encoded_simbols = dictionary();
 
+    % Creazione delle codifiche dei simboli elementari
     for ii = 1:numel(simbols(:,1))
         simbol = simbols(ii,:);
         jj = ii - 1;
         encoded_simbols(simbol) = {double(dec2bin(jj, num_bits_simbols) == '1')};
     end
 
+    % Codifca delle tuple in binario
     for ii = 1:num_phrases
         index = tuples(ii,1);
         simbol = tuples(ii,2);
